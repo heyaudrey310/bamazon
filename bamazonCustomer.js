5eee83a37be6b121cafe41353632b783f539223c
Audrey Agyemang <audreyagyemang310@gmail.com>
2019-04-29 23:41:47 -0400

    add code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var table = require("cli-table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,114 +16,95 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    // console.log("connected as id " + connection.threadId + "\n");
-    displayProduct();
-});
+function productItems() {
+	connection.connect(function(err) {
 
-function displayProduct() {
-    console.log("Displaying a new product...\n");
-    var query = connection.query( 
-        "Select * FROM products",
-        {
-            item: 3402,
-            product: "IPhone X",
-            department: "Electronics",
-            price: 999.00,
-            quantity: 772
-        },
-        {
-            item: 2151,
-            product: "Amazon Fire Stick",
-            department: "Electronics",
-            price: 49.99,
-            quantity: 1035
-        },
-        {
-            item: 1765,
-            product: "Echo Dot",
-            department: "Electronics",
-            price: 49.99,
-            quantity: 2043
-        },
-        {
-            item: 3597,
-            product: "Fujifilm Instant Film",
-            department: "Camera",
-            price: 34.99,
-            quantity: 5498
-        },
-        {
-            item: 9873,
-            product: "Playstation Gift Card",
-            department: "Video Games",
-            price: 25.00,
-            quantity: 203
-        },
-        {
-            item: 5982,
-            product: "Becoming",
-            department: "Books",
-            price: 14.99,
-            quantity: 542
-        },
-        {
-            item: 9231,
-            product: "XBox Wireless Controller",
-            department: "Video Games",
-            price: 89.95,
-            quantity: 203
-        },
-        {
-            item: 1203,
-            product: "Rayban Suglasses",
-            department: "Fashion",
-            price: 99.99,
-            quantity: 987
-        },
-        {
-            item: 1145,
-            product: "Women's Summer Dress",
-            department: "Fashion",
-            price: 19.99,
-            quantity: 7889
-        },
-        {
-            item: 1478,
-            product: "Crocs Men's Shoes",
-            department: "Fashion",
-            price: 30.99,
-            quantity: 241
-        },
-        function (err, res) {
-            console.log(res.affectedRows + " product displayed!\n");
-            search();
-        }
-    );
-
-    // logs the actual query being run
-    console.log(query.sql);
+		connection.query("SELECT * FROM products", function(err, res) {
+		if (err) throw err;
+		else console.table(res , "\n");
+		productId();
+		});
+	});
 }
+productItems();
 
-function search()   {
-    inquirer
-        .prompt({
-            name: "action",
-            type: "list",
-            message: "What would you like to do?",
-            choices: [
-               "What is the ID of the product would you like to buy?",
-               "How many units of the product would you like to buy?",
-               validate: function(product) {
-                if (isNaN(product) === false) {
-                    return true;
-                  }
-                  return false;
-               }
-            ]
-        })
-        
+
+function productId() {
+
+	inquirer.prompt([
+
+		{
+		 type: "input",
+		 name: "id",
+		 message: "What is the ID of the product you would like to buy?.\n",
+		 validate: function(value) {
+		 	if (!isNaN(value) && value < 10) {
+		 		return true;
+		 	}
+		 	return false;
+		 }
+		},
+
+		{
+		 type: "input",
+		 name: "quantity",
+		 message: "How many units of the product would you like to buy? \n",
+		 validate: function(value) {
+		 	if (!isNaN(value)) {
+		 		return true;
+		 	}
+		 	return false;
+			}
+		}
+
+		]).then(function(answer) {
+
+			var userId = answer.id;
+			console.log("Chosen item id: " , userId);
+
+			var userQuantity = answer.quantity;
+			console.log("Chosen quantity from stock: " , userQuantity , "\n");
+
+			connection.query("SELECT * FROM products WHERE ?", [{ item_id : answer.id }], function(err, res) {
+				if (err) throw err;
+				
+				
+				console.table(res);
+				var currQuantity = res[0].stock_quantity;
+				console.log("Current quantity in stock: " , currQuantity);
+				var price = res[0].price;
+				var remQuantity = currQuantity - answer.quant;
+				console.log("Remaining quantity in stock: " , remQuantity);
+
+				if(currQuantity > answer.quant) {
+
+					console.log("Amount Remaining: " + remQuantity);
+					console.log("Total Cost: " + (answer.quantity * price) + "\n");
+
+					connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?",
+                    [
+                    remQuantity, answer.id
+                    ],
+
+					
+						function(err, res){
+							console.table(res);
+						});
+
+					connection.query("SELECT * FROM products", function(err, res) {
+
+						console.log("This is the updated inventory of product items: ");
+						console.log("------------------------------- \n");
+						console.table(res);
+					});
+
+				} else {
+					console.log("Insufficient amounts, please edit your units!");
+				}
+
+			connection.end();
+
+			});
+		})
+
 }
-
-
--- schema.sql
+++ schema.sql
@@ -1,50 +1,49 @@
--- DROP DATABASE IF EXISTS bamazon_db;
+DROP DATABASE IF EXISTS bamazon_db;
 
--- CREATE DATABASE bamazon_db;
+CREATE DATABASE bamazon_db;
 
--- use bamazon_db;
+use bamazon_db;
 
--- CREATE TABLE products (
--- 	id INT NOT NULL AUTO_INCREMENT,
---     item_id INTEGER NOT NULL,
---     product_name VARCHAR(100) NULL,
---     department_name VARCHAR(100) NULL,
---     price DECIMAL(10,2) NULL,
---     stock_quantity INT NULL,
---     PRIMARY KEY(id)
--- );
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(50) NULL,
+    department_name VARCHAR(50) NULL,
+    price DECIMAL(10,2) NULL,
+    stock_quantity INT NULL,
+    PRIMARY KEY(item_id)
+);
 
--- INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
--- VALUES (3402, "IPhone X", "Electronics", 999, 772);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3402, "IPhone X", "Electronics", 999, 772);
 
--- INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
--- VALUES (2151, "Amazon Fire Stick", "Electronics", 49.99, 1035);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (2151, "Amazon Fire Stick", "Electronics", 49.99, 1035);
 
--- INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
--- VALUES ( 1765, "Echo Dot", "Electronics", 49.99, 2043);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ( 1765, "Echo Dot", "Electronics", 49.99, 2043);
 
--- INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
--- VALUES (3597, "Fujifilm Instant Film", "Camera", 34.99, 5498);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3597, "Fujifilm Instant Film", "Camera", 34.99, 5498);
 
--- INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
--- VALUES (9873, "Playstation Gift Card", "Video Games", 25, 1098);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (9873, "Playstation Gift Card", "Video Games", 25, 1098);
 
--- INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
--- VALUES (5982, "Becoming", "Books", 14.99, 542);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (5982, "Becoming", "Books", 14.99, 542);
 
--- INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
--- VALUES (9231, "XBox Wireless Controller", "Video Games", 89.95, 203);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (9231, "XBox Wireless Controller", "Video Games", 89.95, 203);
 
--- INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
--- VALUES (1203, "Rayban Sunglasses", "Fashion", 99.99, 987);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (1203, "Rayban Sunglasses", "Fashion", 99.99, 987);
 
--- INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
--- VALUES (1145, "Women's Summer Dress", "Fashion", 19.99, 7889);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (1145, "Women's Summer Dress", "Fashion", 19.99, 7889);
 
--- INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
--- VALUES (1478, "Crocs Men's Shoes", "Fashion", 30.99, 241);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (1478, "Crocs Men's Shoes", "Fashion", 30.99, 241);
 
--- SELECT * FROM products;
+SELECT * FROM products;
 
 
 
