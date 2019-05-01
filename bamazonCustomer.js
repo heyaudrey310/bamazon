f617deb192d22071825ca3750ac1f3597aa12e56
Audrey Agyemang <audreyagyemang310@gmail.com>
2019-05-01 05:49:51 -0400

    add cli-table2 to terminal

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var table = require("cli-table");
+var Table = require("cli-table2");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,6 +15,31 @@ var connection = mysql.createConnection({
     password: "root",
     database: "bamazon_db"
 });
+
+// went to 'npm cli-table2' so a table of the products can be visible in terminal
+var display = function() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      var table = new Table({
+        head: ["Product Id", "Product Description", "Cost"],
+        colWidths: [12, 50, 8],
+        colAligns: ["center", "left", "right"],
+        style: {
+          head: ["aqua"],
+          compact: true
+          // 'padding-right' : 1,
+        }
+      });
+  
+      for (var i = 0; i < res.length; i++) {
+        table.push([res[i].id, res[i].products_name, res[i].price]);
+      }
+  
+      console.log(table.toString());
+      console.log("");
+      productItems();
+    }); //End Connection to products
+};
 
 function productItems() {
 	connection.connect(function(err) {
